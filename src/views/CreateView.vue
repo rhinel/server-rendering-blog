24981f284639b484094f5aed44efc72b894482ea
rhinel <rhinelkc@hotmail.com>
2017-03-25 00:09:16 +0800

    完善功能

--- src/views/CreateView.vue
+++ src/views/CreateView.vue
@@ -7,33 +7,49 @@
 </template>
 
 <script>
-  let simplemde
-  if (process.env.VUE_ENV == 'client') {
-    simplemde = require('simplemde')
-    require('simplemde/dist/simplemde.min.css')
-    require.ensure([], () => require('github-markdown-css'), 'markdown-style')
-  }
-
   export default {
     name: 'create-view',
     data () {
       return {
-        myeditor: ''
+        myeditor: '',
+        configs: {
+          renderingConfig: {
+            codeSyntaxHighlighting: true,
+            highlightingTheme: 'atom-one-light' // 自定义代码高亮主题，可选列表(https://github.com/isagalaev/highlight.js/tree/master/src/styles)
+          },
+          spellChecker: false
+        }
       }
     },
     mounted () {
-      this.myeditor = new simplemde({
-        element: document.querySelector('.myeditor')[0]
-      })
+      this.initEditor()
       this.addPreviewClass('markdown-body')
+      this.highlight('atom-one-light')
     },
     methods: {
-      addPreviewClass(className) {
+      initEditor () {
+        let configs = {
+          element: document.querySelector('.myeditor')[0]
+        }
+        Object.assign(configs, this.configs)
+        let simplemde = require('simplemde')
+        require('simplemde/dist/simplemde.min.css')
+        require.ensure([], () => require('github-markdown-css'), 'markdown-style')
+        this.myeditor = new simplemde(configs)
+      },
+      addPreviewClass (className) {
         const wrapper = this.myeditor.codemirror.getWrapperElement()
         const preview = document.createElement('div')
         wrapper.nextSibling.className += ` ${className}`
         preview.className = `editor-preview ${className}`
         wrapper.appendChild(preview)
+      },
+      highlight (tm) {
+        require.ensure([], () => {
+          const theme = tm || 'default'
+          window.hljs = require('highlight.js')
+          require(`highlight.js/styles/${theme}.css`)
+        }, 'highlight')
       }
     }
   }
