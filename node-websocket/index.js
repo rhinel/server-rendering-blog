44a5624ce9112e46cd4504ca54f67d0b4d2f2bd2
rhinel <rhinelkc@hotmail.com>
2017-07-26 10:50:53 +0800

    常规更新

--- node-websocket/index.js
+++ node-websocket/index.js
@@ -10,17 +10,17 @@ let clients = []
 let wss = new WebSocketServer({ port: 8181 })
 // 事件
 wss.on('connection', (ws) => {
-	// 记录连接
-	let client_uuid = uuid.v4()
-	clients.push({ 'id': client_uuid, 'ws': ws })
-    console.log(client_uuid + ' client connected')
+  // 记录连接
+  let client_uuid = uuid.v4()
+  clients.push({ 'id': client_uuid, 'ws': ws })
+  console.log(client_uuid + ' client connected')
 
-    // 收到消息后发送给第一个
-    ws.on('message', (message) => {
-    	let clientSocket = clients[0].ws
-    	if (clientSocket.readyState === WebSocket.OPEN) {
-    		clientSocket.send(message)
-    	}
-        console.log(JSON.parse(message))
-    })
+  // 收到消息后发送给第一个
+  ws.on('message', (message) => {
+    let clientSocket = clients[0].ws
+    if (clientSocket.readyState === WebSocket.OPEN) {
+      clientSocket.send(message)
+    }
+    console.log(JSON.parse(message))
+  })
 })
